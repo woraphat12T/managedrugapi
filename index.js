73ce23553dad5f15a443b854d820f983e96e3895
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-08-25 21:54:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const { API_PORT } = process.env
 const port = process.env.port || API_PORT 
  
 
-server.listen(8585,()=>{
+server.listen(8383,()=>{
     console.log(`server is running ${API_PORT}`)
 })
